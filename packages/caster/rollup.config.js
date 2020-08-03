f5273bb62ab3ea9091ae522defa16f4c34e181de
Brett Gullan <brettgullan@mac.com>
2020-08-04 09:16:27 +1000

    feat: Initial rollup config

---
+++ packages/caster/rollup.config.js
@@ -0,0 +1,46 @@
+
+import babel from '@rollup/plugin-babel'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+import analyze from 'rollup-plugin-analyzer'
+
+// ----------------------------------------------------------------------------
+
+import pkg from './package.json'
+
+// ----------------------------------------------------------------------------
+
+const input = 'src/index.js'
+
+// ----------------------------------------------------------------------------
+
+export default [
+  {
+    input,
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+
+    plugins: [
+      nodeResolve({
+        preferBuiltins: true,
+      }),
+      commonjs(),
+      babel({
+        exclude: /node_modules/,
+      }),
+      terser(),
+      analyze({ summaryOnly: true }),
+    ],
+  },
+]
