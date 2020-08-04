21c580ca1479c23caad43204a5732a4bc152bb2f
Brett Gullan <brettgullan@mac.com>
2020-08-04 10:28:06 +1000

    improve: Make ramda, ramda-adjunct and traverse libraries peers
    Otherwise bundle was too large. And given (my) likely use-case scenarios, ramda et al would always be present anyway.

--- packages/caster/rollup.config.js
+++ packages/caster/rollup.config.js
@@ -2,6 +2,7 @@
 import babel from '@rollup/plugin-babel'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import { terser } from 'rollup-plugin-terser'
 import analyze from 'rollup-plugin-analyzer'
 import visualizer from 'rollup-plugin-visualizer'
@@ -34,6 +35,7 @@ export default [
     ],
 
     plugins: [
+      peerDepsExternal(),
       nodeResolve({
         preferBuiltins: true,
       }),
